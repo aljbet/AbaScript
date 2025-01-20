2b21a65981c9b1f8600b96ebd4c9c2aaf1aab4ff
sinedmv <raybeker890@gmail.com>
2025-01-20 13:43:26 +0300

    Добавил все ключевые слова в KeyWords

--- CompileLanguage/CompileClasses/Keywords.cs
+++ CompileLanguage/CompileClasses/Keywords.cs
@@ -1,12 +1,23 @@
 ﻿namespace CompileLanguage.CompileClasses;
 
-public class Keywords
+public static class Keywords
 {
-    public static string STORE = "STORE";
-    public static string PUSH = "PUSH";
-    public static string SUB = "SUB";
-    public static string ADD = "ADD";
-    public static string MUL = "MUL";
-    public static string DIV = "DIV";
-    public static string MOD = "MOD";
+    public const string LOAD = "LOAD";
+    public const string STORE = "STORE";
+    public const string PUSH = "PUSH";
+    public const string POP = "POP";
+    public const string ADD = "ADD";
+    public const string SUB = "SUB";
+    public const string MUL = "MUL";
+    public const string DIV = "DIV";
+    public const string MOD = "MOD";
+    public const string JMP = "JMP";
+    public const string IF_THEN_ELSE = "IF_THEN_ELSE";
+    public const string CALL = "CALL";
+    public const string RET = "RET";
+    public const string PRINT = "PRINT";
+    public const string HALT = "HALT";
+    public const string ENTER_SCOPE = "ENTER_SCOPE";
+    public const string EXIT_SCOPE = "EXIT_SCOPE";
+    public const string INIT = "INIT";
 }
